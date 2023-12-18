b829dc9369acf6d8bf062ec71497f6bcc8ddd039
F-Alexey <f.alexey.workspace@gmail.com>
2023-12-18 22:04:33 +0300

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,19 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
@@ -18,5 +31,11 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Item first = new Item();
+        Item second = new Item("Ivan");
+        Item third = new Item(1, "Anton");
     }
 }
