63e3f322bf9c704f01be022cc5107dd8e344cb62
F-Alexey <149326667+F-Alexey@users.noreply.github.com>
2024-03-31 15:22:17 +0300

    2.0. Лямбда  [#58165]

--- src/main/java/ru/job4j/lambda/LambdaComparator.java
+++ src/main/java/ru/job4j/lambda/LambdaComparator.java
@@ -4,11 +4,11 @@ import java.util.Comparator;
 
 public class LambdaComparator {
     public static void main(String[] args) {
-        Comparator<String> comparatorText = (left, right) -> right.compareTo(left);
-        Comparator<String> comparatorDescSize = (left, right) -> Integer.compare(left.length(), right.length());
+        Comparator<String> comparatorText = (left, right) -> left.compareTo(right);
+        Comparator<String> comparatorDescSize = (left, right) -> Integer.compare(right.length(), left.length());
         String left = ("efghijk");
         String right = ("abcd");
-        System.out.println(comparatorText.compare(left, right) > 0 ? left + "; " + right + " sort by alphabet" : right + "; " + left + " sort by alphabet");
-        System.out.println(comparatorDescSize.compare(left, right) > 0 ? left + "; " + right + " sort by descSize" : right + "; " + left + " sort by descSize");
+        System.out.println(comparatorText.compare(left, right) < 0 ? left + "; " + right + " sort by alphabet" : right + "; " + left + " sort by alphabet");
+        System.out.println(comparatorDescSize.compare(left, right) < 0 ? left + "; " + right + " sort by descSize" : right + "; " + left + " sort by descSize");
     }
 }
